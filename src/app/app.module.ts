fe1be90b4ab3d44249d810b3cfa2cbe1d0415826
James Bell <jam3s8ell@gmail.com>
2017-01-25 19:13:41 +0000

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
+/*
+* to install InMemoryWebApiModule/InMemoryDataService
+* enter the following from the project folder
+* npm i angular-in-memory-web-api
+*/
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -10,18 +18,23 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroService } from './services/hero.service';
 
 import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+import './rxjs-extensions';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+	InMemoryWebApiModule.forRoot(InMemoryDataService),
 	AppRoutingModule
   ],
   providers: [{ provide: HeroService, useClass: HeroService }],
